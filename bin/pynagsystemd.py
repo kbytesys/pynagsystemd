80b02bd8343ee295a698c89aad5db799a4d494ec
kbyte <kbytesys@gmail.com>
2015-07-15 17:54:59 +0200

    pypi ready
    Signed-off-by: kbyte <kbytesys@gmail.com>

--- bin/pynagsystemd.py
+++ bin/pynagsystemd.py
@@ -1,3 +1,4 @@
+#!/bin/python
 """
 You can redistribute it and/or modify it under the terms of the GNU General
 Public License as published by the Free Software Foundation, either version 2
---
+++ setup.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+setup(
+    name='pynagsystemd',
+    packages=find_packages(),
+    version='1.0.0',
+    scripts=['bin/pynagsystemd.py'],
+    install_requires=[
+        'nagiosplugin>=1.2',
+    ],
+    description='A simple nagios plugin that detects failed systemd units.',
+    author='Andrea Briganti',
+    author_email='kbytesys@gmail.com',
+    url='https://github.com/kbytesys/pynagsystemd',
+    download_url='https://github.com/kbytesys/pynagsystemd/tarball/v1.0.0',
+    keywords=['nagios', 'systemd'],
+    license='GNU LGPL v2',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Plugins',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: GNU Lesser General Public License v2 (LGPLv2)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        #Programming Language :: Python :: 3 :: Only
+        'Operating System :: POSIX :: Linux',
+        'Topic :: System :: Networking :: Monitoring'
+    ],
+)
+
+
