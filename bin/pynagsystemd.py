2c9e8353d6a8dac730b95e3750c11e9f3e893a03
Andrea Briganti <kbytesys@gmail.com>
2016-03-14 12:29:25 +0100

    Fix python path

--- bin/pynagsystemd.py
+++ bin/pynagsystemd.py
@@ -1,4 +1,4 @@
-#!/bin/python
+#!/usr/bin/env python
 """
 You can redistribute it and/or modify it under the terms of the GNU General
 Public License as published by the Free Software Foundation, either version 2
