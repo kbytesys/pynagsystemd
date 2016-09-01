a1d2af0a78015684b554c83604d572ce43b6b283
Felix Buehler <felix.christian.buehler@web.de>
2016-09-01 11:13:39 +0200

    Added Parsing option for testing a single Systemd-service

--- bin/pynagsystemd.py
+++ bin/pynagsystemd.py
@@ -8,8 +8,18 @@ Copyright Andrea Briganti a.k.a 'Kbyte'
 """
 import io
 import subprocess
+from optparse import OptionParser
 
 import nagiosplugin
+
+
+parser = OptionParser()
+parser.add_option("-s", "--service", type="string", dest="service",
+                  help="Name of the Service that is beeing tested")
+
+global service
+(options, args) = parser.parse_args()
+service = str(options.service)
 
 
 class SystemdStatus(nagiosplugin.Resource):
@@ -39,6 +49,36 @@ class SystemdStatus(nagiosplugin.Resource):
         return [nagiosplugin.Metric('systemd', (True, None), context='systemd')]
 
 
+class ServiceStatus(nagiosplugin.Resource):
+    name = 'SYSTEMD'
+
+    def probe(self):
+        # Execute systemctl is-active and get output
+        global service
+        try:
+            p = subprocess.Popen(['systemctl', 'is-active', service],
+                                 stderr=subprocess.PIPE,
+                                 stdin=subprocess.PIPE,
+                                 stdout=subprocess.PIPE)
+            pres, err = p.communicate()
+        except OSError as e:
+            raise nagiosplugin.CheckError(e)
+
+        if err:
+            raise nagiosplugin.CheckError(err)
+        if pres:
+            result = ""
+            for line in io.StringIO(pres.decode('utf-8')):
+                result = "%s %s" % (result, line.split(' ')[0])
+            result = result.strip()
+            if result == "active":
+                return [nagiosplugin.Metric('systemd', (True, None), context='systemd')]
+            else:
+                return [nagiosplugin.Metric('systemd', (False, service), context='systemd')]
+
+        return [nagiosplugin.Metric('systemd', (False, "No Service given"), context='systemd')]
+
+
 class SystemdContext(nagiosplugin.Context):
     def __init__(self):
         super(SystemdContext, self).__init__('systemd')
@@ -52,10 +92,22 @@ class SystemdContext(nagiosplugin.Context):
 
 
 def main():
-    check = nagiosplugin.Check(
-        SystemdStatus(),
-        SystemdContext())
+    parser = OptionParser()
+    parser.add_option("-s", "--service", type="string", dest="service",
+                       help="Name of the Service that is beeing tested")
 
+    global service
+    (options, args) = parser.parse_args()
+    service = str(options.service)
+
+    if service == "None":
+        check = nagiosplugin.Check(
+            SystemdStatus(),
+            SystemdContext())
+    else:
+        check = nagiosplugin.Check(
+            ServiceStatus(),
+            SystemdContext())
     check.main()
 
 
